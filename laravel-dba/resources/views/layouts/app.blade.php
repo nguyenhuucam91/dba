25b277da3876af4090baf557e3c0f0c7033f88bf
camnh <nguyenhuucam91@gmail.com>
2020-12-29 22:46:40 +0700

    Update user_profile

--- laravel-dba/resources/views/layouts/app.blade.php
+++ laravel-dba/resources/views/layouts/app.blade.php
@@ -59,6 +59,11 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     {{-- Edit profile --}}
+                                    <a class="dropdown-item" href="/user-profile">
+                                        Edit profile
+                                    </a>
+
+                                    {{-- Logout --}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
