dc912546bff71ca7b3f1c4b18416fb75c9d92552
camnh <nguyenhuucam91@gmail.com>
2021-01-04 16:08:47 +0700

    Remove content of mongodb folder

---
+++ laravel-dba/app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserProfile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
+
+class UserProfileController extends Controller
+{
+    public function index()
+    {
+        $user = null;
+        //if key exists, then get data from Redis key
+        if (Redis::exists('user_profile:'.Auth::user()->id)) {
+            $user = Redis::hgetall('user_profile:'.Auth::user()->id);
+        }
+        // if key is not exist, we try to find user_information associated with authenticated user id
+        else {
+            $user = UserProfile::where(['user_id' => Auth::user()->id])->first();
+            //if there is no user profile, we create a new object and cast to array,
+            //since redis result returns array
+            if ($user === null) {
+                $user = new UserProfile;
+            }
+            $user = $user->toArray();
+        }
+        //push all data to view
+        return view('user-profile.index', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $userId = Auth::user()->id;
+        //merge form data which user inputs with authenticated user_id to update
+        $dataToUpdate = array_merge($request->except(['_token']), ['user_id' => $userId]);
+        //find whether user exist inside database or not
+        $userProfile = UserProfile::where(['user_id' => $userId])->first();
+        // if userProfile exist, then we update db
+        if ($userProfile) {
+            $userProfile->update($dataToUpdate);
+        }
+        // if not exist, then we create new profile for that user
+        else {
+            UserProfile::create($dataToUpdate);
+        }
+        //copy this data to cache for later retrieval
+        Redis::hmset('user_profile:'. Auth::user()->id, $userProfile->toArray());
+
+        return \redirect()->action([UserProfileController::class, 'index']);
+    }
+}
---
+++ laravel-dba/resources/views/students/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Students</h1>
+    <p><a href="/students/create" class="btn btn-primary">Create new student</a></p>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Student ID</th>
+                <th>Student Name</th>
+                <th>Student Dob</th>
+                <th>Student Phone number</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- Foreach: loop through each element in collection|array, then get field from these attribute --}}
+            @foreach ($students as $student)
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->dob }}</td>
+                    <td>{{ $student->phone_number }}</td>
+                    <td>
+                        <a href="/students/{{ $student->id }}/edit">Edit</a>
+                        <a href="javascript:void(0)" onclick="confirm('Are you sure') ? document.getElementById('student-delete-form-{{ $student->id }}').submit() : null">Delete</a>
+                        <form action="/students/{{ $student->id }}" id="student-delete-form-{{ $student->id }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
