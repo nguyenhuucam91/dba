3677021a7966fb28ddf6ec37fe32f73be1b0d6f6
camnh <nguyenhuucam91@gmail.com>
2021-01-07 19:49:59 +0700

    Add studentController code

--- laravel-dba/app/Student.php
+++ laravel-dba/app/Student.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Student extends Model
 {
     // allow all attributes to be pushed into Student model
