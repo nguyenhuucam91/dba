9884121513bc2b19eaf69f6b051e783fecc20ff7
camnh <nguyenhuucam91@gmail.com>
2021-02-20 10:23:34 +0700

    Update tut12

--- laravel-dba/config/database.php
+++ laravel-dba/config/database.php
@@ -95,9 +95,9 @@ return [
             'driver' => 'mongodb',
             'host' => env('DB_HOST', 'mongo'),
             'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'homestead'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
+            'database' => env('DB_DATABASE', 'laravel-mongodb'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'example'),
             'options' => [
                 // here you can pass more settings to the Mongo Driver Manager
                 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
